5013d23f2cffc17c80dc63bcf7671a53831cfcd8
salman98kaji <salman2017kaji@gmail.com>
2025-01-04 23:28:10 +0530

    fist commit

---
+++ pftFrontend/src/utils/axiosInstance.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8000/api/',
+    timeout: 5000,
+    headers: {
+        "Content-Type": "application/json",
+    }
+});
