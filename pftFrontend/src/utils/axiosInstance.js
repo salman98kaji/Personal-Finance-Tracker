faf11d471a2164dadb213ea8a7e58f5aaec6ba09
salman98kaji <salman2017kaji@gmail.com>
2025-01-06 00:31:42 +0530

    Remove frontend folder from backend branch

--- pftFrontend/src/utils/axiosInstance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/',
-    timeout: 5000,
-    headers: {
-        "Content-Type": "application/json",
-    }
-});
