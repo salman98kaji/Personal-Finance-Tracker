3feea868e53a7a3f573b96c71b75c9d8d4c3bef4
salman98kaji <salman2017kaji@gmail.com>
2025-01-08 22:35:41 +0530

    jwt added

--- financeTracker/src/main/java/com/example/Controller/UserController.java
+++ financeTracker/src/main/java/com/example/Controller/UserController.java
@@ -2,9 +2,15 @@ package com.example.Controller;
 
 import com.example.DTO.UserRequestDTO;
 import com.example.DTO.UserResponseDTO;
+import com.example.Service.UserDetailsServiceImpl;
 import com.example.Service.UserService;
+import com.example.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,10 +20,38 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserRequestDTO userRequestDTO){
         UserResponseDTO userResponse = userService.registerUser(userRequestDTO);
         return ResponseEntity.ok(userResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserRequestDTO userRequestDTO){
+        // Authenticate the user
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userRequestDTO.getUsername(),
+                        userRequestDTO.getPassword()
+                )
+        );
+
+        // Load user details
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userRequestDTO.getUsername());
+
+        // Generate JWT token
+        String jwt = jwtUtil.generateToken(userDetails.getUsername());
+
+        return new ResponseEntity<>(jwt, HttpStatus.OK);
     }
 
     @GetMapping("/id/{userId}")
--- financeTracker/src/main/java/com/example/financeTracker/SecurityConfig.java
+++ financeTracker/src/main/java/com/example/financeTracker/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.example.financeTracker;
 
 import com.example.Service.UserDetailsServiceImpl;
+import com.example.utils.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,10 +26,13 @@ public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(request -> request
-                        .requestMatchers("/api/users/register").permitAll()
+                        .requestMatchers("/api/users/register","/api/users/login").permitAll()
                         .requestMatchers("/api/users/username/**").authenticated()
                         .requestMatchers("/api/users/id/**").authenticated()
                         .requestMatchers("/api/accounts/**").authenticated()
@@ -33,13 +40,19 @@ public class SecurityConfig {
                         .requestMatchers("/api/category/**").authenticated()
                         .requestMatchers("/api/transactions/**").authenticated()
                         .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Autowired
-     void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception{
+        return auth.getAuthenticationManager();
+    }
+
 }
